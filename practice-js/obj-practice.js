a21298538d7c8e17255ea839a7ec44f598f6e32f
takahashi daiken <258454@st.takushoku-u.ac.jp>
2026-06-22 18:46:09 +0900

    JSを初め学んだ。

--- practice-js/obj-practice.js
+++ practice-js/obj-practice.js
@@ -15,4 +15,10 @@ let gakka = [
 ];
 
 //////////////// ここから下にプログラムを書きたそう!
-
+console.log(campus.address);
+for(let r of campus.buildingD){
+console.log(r);
+}
+for(let a of gakka){
+console.log(a.name);
+}
